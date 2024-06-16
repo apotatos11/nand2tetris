da8f6162c0a5f65d7713bdc741ebbead5c539c87
Wonil Nam <wonil.nam@voyagerx.com>
2024-06-16 12:35:50 +0900

    Apply symbol table

--- projects/6/utils.js
+++ projects/6/utils.js
@@ -5,15 +5,55 @@ const {
   changeACommandTo16BitBinary,
   changeCCommandTo16BitBinary,
 } = require('./parser');
+const { SYMBOL_TABLE } = require('./constant');
 
-const printFileLineByLine = async (filePath) => {
+const updateLabelLineByLine = async (filePath) => {
   const fileStream = fs.createReadStream(filePath);
-
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
   });
 
+  let currentLineNumber = 0;
+
+  for await (const line of rl) {
+    const trimmedLine = line.trim();
+
+    const isComment = trimmedLine.startsWith('//');
+    const isWhiteSpace = trimmedLine.length === 0;
+    const isACommand = trimmedLine.startsWith('@');
+    const isLabel = trimmedLine.startsWith('(');
+
+    if (isComment) {
+      continue;
+    }
+
+    if (isWhiteSpace) {
+      continue;
+    }
+
+    if (isLabel) {
+      const labelName = trimmedLine.slice(1, -1);
+      SYMBOL_TABLE.set(labelName, currentLineNumber + 1);
+    }
+
+    if (isACommand) {
+      currentLineNumber += 1;
+    } else {
+      // CComaand일대
+      currentLineNumber += 1;
+    }
+  }
+};
+
+const printFileLineByLine = async (filePath) => {
+  const fileStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  let currentVariableValue = 16;
   let outputData = '';
   const binaryLines = [];
 
@@ -36,7 +76,32 @@ const printFileLineByLine = async (filePath) => {
 
     if (isACommand) {
       const aCommand = trimmedLine.slice(1);
-      binaryLine = changeACommandTo16BitBinary(aCommand);
+      const isACommandNumber = !isNaN(Number(aCommand));
+
+      if (isACommandNumber) {
+        binaryLine = changeACommandTo16BitBinary(aCommand);
+        binaryLines.push(binaryLine);
+
+        continue;
+      }
+
+      const hasSymbolInTable = SYMBOL_TABLE.has(aCommand);
+
+      if (hasSymbolInTable) {
+        // aCommand 기호가 있는지 기호테이블에서 조회
+        // 있으면 기호에 대응하는 숫자값으로 교체
+        const targetSymbol = aCommand;
+        const aCommandNumber = SYMBOL_TABLE.get(targetSymbol);
+        binaryLine = changeACommandTo16BitBinary(aCommandNumber);
+      } else {
+        // 테이블에 없다면 그 기호는 새로운 변수
+        // 새로운 변수 기호테이블에 담기
+        // 새로운 변수 숫자로 변환해서 처리하기.
+        const newSymbol = aCommand;
+        SYMBOL_TABLE.set(newSymbol, currentVariableValue);
+        binaryLine = changeACommandTo16BitBinary(currentVariableValue);
+        currentVariableValue += 1;
+      }
     } else {
       binaryLine = changeCCommandTo16BitBinary(trimmedLine);
     }
@@ -49,6 +114,7 @@ const printFileLineByLine = async (filePath) => {
 
 const readAndProcessFiles = async (inputFilePath, outputFilePath) => {
   try {
+    await updateLabelLineByLine(inputFilePath);
     const outputData = await printFileLineByLine(inputFilePath);
 
     fs.writeFile(outputFilePath, outputData, 'utf8', (err) => {
